539b3be06fdb7dd0c8c5e7fc11b81e0ab7556ee3
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2016-12-07 09:16:52 -0600

    Probando funcionalidad con sqlite

--- js/functions.js
+++ js/functions.js
@@ -25,18 +25,23 @@ function crearBD(db, borrar = false){
 			tx.executeSql('DROP TABLE IF EXISTS asistencia');
 			tx.executeSql('DROP TABLE IF EXISTS justificacion');
 		}
-		tx.executeSql("PRAGMA foreign_keys=ON;", [], function(){}, errorDB);
+		
+		tx.executeSql("PRAGMA foreign_keys=ON;", [], function(){
+			tx.executeSql("PRAGMA foreign_keys;", [], function(tx, res){
+	            console.log('foreign_keys:', res.rows.item(0).foreign_keys == 0?"No":"Si"); // foreign_key: 0
+	        });
+		}, errorDB);
 		
 		tx.executeSql('CREATE TABLE IF NOT EXISTS grupo (idGrupo integer primary key, nombre text, sede text, encargado text)', [], function(){
 			console.log("tabla grupo creada");
-			tx.executeSql('CREATE TABLE IF NOT EXISTS participante (idParticipante integer primary key autoincrement, num_personal integer, idGrupo integer REFERENCES grupo(idGrupo) on update cascade on delete cascade, nombre text, fotografia text, idPlantel integer, nombrePlantel text, plaza text, especialidad text)', [], function(){
+			tx.executeSql('CREATE TABLE IF NOT EXISTS participante (idParticipante integer primary key autoincrement, num_personal integer, idGrupo integer, nombre text, fotografia text, idPlantel integer, nombrePlantel text, plaza text, especialidad text, FOREIGN KEY(idGrupo) REFERENCES grupo(idGrupo) ON UPDATE cascade ON DELETE cascade)', [], function(){
 				console.log("tabla participante creada");
 				
-				tx.executeSql('CREATE TABLE IF NOT EXISTS asistencia(fecha text, idParticipante integer REFERENCES participante(idParticipante) on update cascade on delete cascade, primary key(fecha, idParticipante))', [], function(){
+				tx.executeSql('CREATE TABLE IF NOT EXISTS asistencia(fecha text, idParticipante integer, primary key(fecha, idParticipante), FOREIGN KEY(idParticipante) REFERENCES participante(idParticipante) ON UPDATE cascade ON DELETE cascade)', [], function(){
 					console.log("tabla asistencia creada");
 				}, errorDB);
 				
-				tx.executeSql('CREATE TABLE IF NOT EXISTS justificacion(fecha text, idParticipante integer REFERENCES participante(idParticipante) on update cascade on delete cascade, motivo text, comprobante blob, primary key(fecha, idParticipante))', [], function(){
+				tx.executeSql('CREATE TABLE IF NOT EXISTS justificacion(fecha text, idParticipante integer, motivo text, comprobante blob, primary key(fecha, idParticipante), FOREIGN KEY(idParticipante) REFERENCES participante(idParticipante) ON UPDATE cascade ON DELETE cascade)', [], function(){
 					console.log("tabla justificacion creada");
 				}, errorDB);
 		
